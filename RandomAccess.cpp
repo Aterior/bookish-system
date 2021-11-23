831624869e4fcc89090e32cbc6cd8134ab4ad5bb
LordZool <60788076+LordZool@users.noreply.github.com>
2021-11-23 13:24:18 +0100

    RandomAccess.cpp finished

--- RandomAccess.cpp
+++ RandomAccess.cpp
@@ -68,9 +68,8 @@ int main() {
   float_v tmp2;
   //TODO gather data with indices "index" from the array "input" into float_v tmp2, if the value of "input" large then 0.5
   // Use void  gather (const float *array, const uint_v &indexes, const float_m &mask)
-  uint_v &tmp_index2=reinterpret_cast<uint_v&>(index);
-  const float_m mask = (input > 0.5);
-  tmp2.gather(input, tmp_index2, mask);
+  const float_m mask = (tmp > 0.5f);
+  tmp2.gather(input, tmp_index, mask);
   
   //check results
   ok = 1;
@@ -92,12 +91,12 @@ int main() {
   if(ok) cout << "correct." << endl;
   else   cout << "WRONG." << endl;
   
+  //scatter with masking
   //TODO create mask for values for an obtained tmp values, which are large than 0.5 and
   //TODO put all values smaller than 0.5 from tmp to the array "output" at the places given by indices "index"
   // Use void scatter (float *array, const uint_v &indexes, const float_m &mask) const
-  float_m mask2 = (tmp < 0.5);
-  uint_v &tmp_index3=reinterpret_cast<uint_v&>(index);
-  tmp.scatter(output, tmp_index3, mask);
+  float_m mask2 = (tmp < 0.5f);
+  tmp.scatter(output, tmp_index, mask2);
 
     //check results
   ok = 1;
